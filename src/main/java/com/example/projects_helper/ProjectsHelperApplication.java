f06e8667b47c5de5ca62b851e52cdb9d1bd2fcc8
Adam Sudoł <01158935@pw.edu.pl>
2023-08-31 21:39:45 +0200

    initial intellij commit

---
+++ src/main/java/com/example/projects_helper/ProjectsHelperApplication.java
@@ -0,0 +1,13 @@
+package com.example.projects_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectsHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projects_helper/ProjectsHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projects_helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
