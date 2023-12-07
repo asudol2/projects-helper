700d10cca34f55f38744d369c57d3d290015973b
Adam Sudoł <01158935@pw.edu.pl>
2023-12-07 01:18:10 +0100

    small logic correction

--- backend/src/main/java/pl/thesis/projects_helper/utils/URLArgsUtils.java
+++ backend/src/main/java/pl/thesis/projects_helper/utils/URLArgsUtils.java
@@ -19,7 +19,7 @@ import org.springframework.web.client.RestTemplate;
 
 public class URLArgsUtils {
     private static final Logger logger = LoggerFactory.getLogger(URLArgsUtils.class);
-
+    private static final OAuthConsumer consumer = new DefaultOAuthConsumer("${consumer.key}", "${consumer.secret}");
 
     public static String generateArgsUrl(Map<String, List<String>> args) {
         StringBuilder builder = new StringBuilder();
@@ -39,20 +39,17 @@ public class URLArgsUtils {
         return builder.toString();
     }
 
-    public static String generateSignedUrl(String url, String token, String secret,
-                                           String consumerKey, String consumerSecret)
+    public static String generateSignedUrl(String url, String token, String secret)
             throws OAuthMessageSignerException, OAuthExpectationFailedException, OAuthCommunicationException {
-        OAuthConsumer consumer = new DefaultOAuthConsumer(consumerKey, consumerSecret);
         consumer.setTokenWithSecret(token, secret);
         return consumer.sign(url.replace("|", "%7c")).replace("%7c", "|");
     }
 
-    public static JsonNode requestOnEndpoint(RestTemplate restTemplate, String token, String secret, String baseUrl,
-                                             String consumerKey, String consumerSecret) {
+    public static JsonNode requestOnEndpoint(RestTemplate restTemplate, String token, String secret, String baseUrl) {
         ObjectMapper mapper = new ObjectMapper();
         JsonNode jsonOutput = mapper.createObjectNode();
         try {
-            String signedUrl = generateSignedUrl(baseUrl, token, secret, consumerKey, consumerSecret);
+            String signedUrl = generateSignedUrl(baseUrl, token, secret);
             ResponseEntity<String> response = restTemplate.exchange(signedUrl, HttpMethod.GET, null, String.class);
             jsonOutput = mapper.readTree(response.getBody());
         } catch (Exception e) {
