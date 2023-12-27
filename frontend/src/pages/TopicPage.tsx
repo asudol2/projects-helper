7856c170b4fd4484a464f53757434f0908924543
Daniel <danielkobialka23@gmail.com>
2023-12-27 13:05:04 +0100

    Implemented displaying teams assigned to user at TopicPage

--- frontend/src/pages/TopicPage.tsx
+++ frontend/src/pages/TopicPage.tsx
@@ -20,8 +20,10 @@ export default function TopicPage() {
     const { token, setToken, secret, setSecret } = useUsosTokens();
     const [topic, setTopic] = useState<Topic | null>(null);
     const [creatingTeam, setCreatingTeam] = useState<boolean>(false);
-    const [loading, setLoading] = useState<boolean>(false);
+    const [loadingTeamRequests, setLoadingTeamRequests] = useState<boolean>(false);
+    const [loadingTeams, setLoadingTeams] = useState<boolean>(false);
     const [teamRequests, setTeamRequests] = useState<TeamRequestResponse[]>([]);
+    const [teams, setTeams] = useState<TeamRequestResponse[]>([]);
     const navigate = useNavigate();
 
 
@@ -33,6 +35,7 @@ export default function TopicPage() {
                         setTopic(data);
                         if (!data.temporary) {
                             getTopicTeamRequests(token, secret);
+                            getTopicTeams(token, secret);
                         }
                     }
                 })
@@ -65,12 +68,18 @@ export default function TopicPage() {
         })
     }
 
-    const getTopicTeamRequests = (token: string, secret: string) => {
-        setLoading(true);
-        Requests.getUserTeamRequests(token, secret).then(res => res.res).then(data => {
+    const loadTopicTeamsOrTeamRequests = (
+        token: string,
+        secret: string,
+        loadRequests: boolean,
+        loadingCallback: (arg: boolean) => void, 
+        resultCallback: (arg: TeamRequestResponse[]) => void
+    ) => {
+        loadingCallback(true);
+        Requests.getUserTeamsOrTeamRequests(token, secret, loadRequests).then(res => res.res).then(data => {
             if (data !== undefined) {
                 const result = data.filter(item => String(item.topicId) == topicId);
-                setTeamRequests(result);
+                resultCallback(result);
             } else {
                 SecurityHelper.clearStorage();
                 navigate("/login");
@@ -81,8 +90,16 @@ export default function TopicPage() {
             navigate("/login");
         })
         .finally(() => {
-            setLoading(false);
+            loadingCallback(false);
         });
+    };
+
+    const getTopicTeamRequests = (token: string, secret: string) => {
+        loadTopicTeamsOrTeamRequests(token, secret, true, setLoadingTeamRequests, setTeamRequests);
+    }
+
+    const getTopicTeams = (token: string, secret: string) => {
+        loadTopicTeamsOrTeamRequests(token, secret, false, setLoadingTeams, setTeams);
     }
 
     const teamCreated = () => {
@@ -90,6 +107,8 @@ export default function TopicPage() {
         if (token && secret && !topic?.temporary) {
             setTeamRequests([]);
             getTopicTeamRequests(token, secret);
+            setTeams([]);
+            getTopicTeams(token, secret);
         }
     }
 
@@ -115,13 +134,25 @@ export default function TopicPage() {
                     </div>
                     {creatingTeam &&
                         <CreateTeamComponent courseId={String(topic?.courseID)} title={String(topic?.title)}
-                            callback={teamCreated} />}
-                    {   loading &&
+                            callback={teamCreated} />
+                    }
+                    {   loadingTeams &&
                         <LoadingComponent text="Ładowanie zespołów"/>
+                    }
+                    {!creatingTeam && teams.length > 0 &&
+                            <TopicTeamsComponent
+                                key={0}
+                                teamRequests={teams}
+                                title={"Zespoły, których członkiem jesteś:"}
+                                confirmed={true}
+                            />
+                    }
+                    {   loadingTeamRequests &&
+                        <LoadingComponent text="Ładowanie propozycji zespołów"/>
                     }
                     {!creatingTeam && teamRequests.length > 0 &&
                             <TopicTeamsComponent
-                                key={0}
+                                key={1}
                                 teamRequests={teamRequests}
                                 title={"Propozycje zespołów na ten temat, których członkiem jesteś:"}
                             />
